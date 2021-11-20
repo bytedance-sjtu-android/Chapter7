6465eedcf6511f8d68c072a8c0ca33a6cbeb257b
zhuqingying <zhuqingying@bytedance.com>
2021-11-20 21:23:04 +0800

    calculate sample size

--- AndroidStudy/app/src/main/java/com/domker/study/androidstudy/ImageActivity.kt
+++ AndroidStudy/app/src/main/java/com/domker/study/androidstudy/ImageActivity.kt
@@ -241,17 +241,10 @@ class ImageActivity : AppCompatActivity() {
     }
 
     private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
-        val height = options.outHeight
-        val width = options.outWidth
-        var inSampleSize = 1
-        if (height > reqHeight || width > reqWidth) {
-            val halfHeight = height / 2
-            val halfWidth = width / 2
-            while (halfHeight / inSampleSize >= reqHeight && halfWidth / inSampleSize >= reqWidth) {
-                inSampleSize *= 2
-            }
-        }
-        return inSampleSize
+        /**
+         * todo calculate sampleSize
+         */
+        return 1
     }
 
     companion object {
